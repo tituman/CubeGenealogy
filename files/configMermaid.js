ca46603f44e2375088ebe969e6cea69387fdf215
tituman <tituman@users.noreply.github.com>
2022-01-19 14:26:59 +0900

    favicon

--- files/configMermaid.js
+++ files/configMermaid.js
@@ -1,12 +1,22 @@
-    var config = {
-        startOnLoad:true,
-        flowchart:{
-            useMaxWidth:true,
-            htmlLabels:true,
-            //curve:'cardinal',
-        },
-        securityLevel:'loose',
-		'theme' : 'dark'
-    };
+function setFavicons(favImg){
+    let headTitle = document.querySelector('head');
+    let setFavicon = document.createElement('link');
+    setFavicon.setAttribute('rel','shortcut icon');
+    setFavicon.setAttribute('href',favImg);
+    headTitle.appendChild(setFavicon);
+}
+setFavicons('files/favicon.png');
 
-    mermaid.initialize(config);
+
+var config = {
+	startOnLoad:true,
+	flowchart:{
+		useMaxWidth:true,
+		htmlLabels:true,
+		//curve:'cardinal',
+	},
+	securityLevel:'loose',
+	'theme' : 'dark'
+};
+
+mermaid.initialize(config);
