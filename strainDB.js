45bd1acc9c33d2caea0cf4629104fc8a85811c9b
tituman <tituman@users.noreply.github.com>
2022-02-02 15:06:12 +0900

    Update strainDB.js

--- strainDB.js
+++ strainDB.js
@@ -1,53 +1,140 @@
+// to make more readable, a backslash can be typed right before a newline, while inside the string quotes
+
 const strainDB = {
  'OG' : 'OG Ps cubexsis'
-,'B+' : '(original genetics)'
-,'PE' : '<p>(original genetics),</p><p>Developed by Rich G, from a blue abort targeting more blueing, from "Amazon" stock</p><p>OLD/wrong info ==> probably by Pollock + McKenna from amazonian/colombian stock</p>'
+ 
+,'B+' : 'B+ (original genetics)'
+
+,'PE' : '<p>PE: (original genetics),</p>\
+<p>Developed by Rich G, from a blue abort and targeting more blueing, from "Amazon" stock</p>\
+<p>Rich Gee: "The PE was born of bad genetics"</p>\
+<p>OLD/wrong info ==> probably by Pollock + McKenna from amazonian/colombian stock</p>'
+
 ,'Melmac' : 'iso of original homestead PE'
+
 ,'KSSS' : 'Koh Samui Super Strain, original genetics'
+
 ,'GT' : 'Golden Teacher, original genetics'
+
 ,'Yimir' : 'info?'
+
 ,'Mr_Krinkle' : 'derivative? isolation? strain? info?'
+
 ,'Choco_Krinkle' : 'Choco Krinkle: Mr. Krinkle x El Choco'
+
 ,'CK_Brains' : 'CK Brains: Mr. Krinkle x El Choco'
+
 ,'Leng' : 'info?'
+
 ,'Ground_Zero' : 'Ground Zero: Leng x Choda"'
+
 ,'Wombat_TAT' : "Dave Wombat's isolation from unstable TAT g.t genetics"
-,'Jack_Frost' : 'Jack Frost: (W)TAT x APE by Dave Wombat<p>a beautiful albino fruit that when mature bows the cap almost completely upside down revealing the blueish grey gills..</p><p></p>'
+
+,'Jack_Frost' : '<p>Jack Frost: (W)TAT x APE by Dave Wombat</p>\
+<p>a beautiful albino fruit that when mature bows the cap almost completely upside down revealing the blueish grey gills..</p>\
+<p></p>'
+
 ,'Albino_KSAT' : 'Albino KSAT: TAT x KSSS'
+
 ,'Golden_KSAT' : 'Golden KSAT: TAT x KSSS'
-,'TAT' : 'OG TAT: True Albino Teacher<p>GT Mutation</p><p>The average cube shape with a true albino mutation ...coming from a unstable g.t grow</p><p>By Jik Fibs</p><p></p>'
+
+,'TAT' : '<p>OG TAT: True Albino Teacher</p>\
+<p>GT Mutation</p>\
+<p>The average cube shape with a true albino mutation ...coming from a unstable g.t grow</p>\
+<p>By Jik Fibs</p>\
+<p></p>'
+
 ,'Toad' : 'info?'
+
 ,'RR' : 'RogerRabbits: Leucistic TAT'
-,'Yeti' : '<p>A slow growing mutation of tat...with a penis shaped fruit</p><p>a large p.e looking fruit with great yeilds</p><p>By Jik Fibs</p><p></p>'
+
+,'Yeti' : '<p>A slow growing mutation of tat...with a penis shaped fruit</p>\
+<p>a large p.e looking fruit with great yeilds</p>\
+<p>By Jik Fibs</p>\
+<p></p>'
+
 ,'Nutcracker' : 'info?'
+
 ,'Yeti2' : 'info?'
+
 ,'Deep_End' : 'info?'
+
 ,'Clockwerk_TAT' : 'info?'
+
 ,'Colckwerk_Orange' : 'info? revert?'
+
 ,'NST' : 'info?'
-,'Ghost' : '<p>A slow growing mutation of tat...small fruits with a spoty pin set...first flush and second flush can over lap...with lots of aborts ...definitely not grown for its yeilds...but the potancy is far above average</p><p>a smaller fat bottom stipe with a wide wavy cap</p><p>By Jik Fibs</p><p></p>'
+
+,'Ghost' : '<p>Ghost: <p>\
+<p>A slow growing mutation of tat...small fruits with a spoty pin set...first flush and second flush can over lap...with lots of aborts ...definitely not grown for its yeilds...but the potancy is far above average</p>\
+<p>a smaller fat bottom stipe with a wide wavy cap</p>\
+<p>By Jik Fibs</p>\
+<p></p>'
+
 ,'TAT_Black_Cap' : 'info?'
+
 ,'Harlequin' : 'info?'
+
 ,'Toad_Black_Cap' : 'Toad Black Cap: Toad x TAT Black Cap'
+
 ,'Riptide' : 'Riptide: TAT x TW'
+
 ,'A_Riptide' : '? Albino Riptide'
-,'TAM' : 'TAM: <p>TAM project: TAT x Melmac</p>'
+
+,'TAM' : '<p>TAM: </p>\
+<p>TAM project: TAT x Melmac</p>'
+
 ,'XXX' : 'info?'
-,'Pearly_Gates' : '<p>An isolation of the t.a.m project Michael Montgomery has worked on...with full tubs first flush.. beautiful blue caps and a pearly shimmer to the lumpy stipes..</p>'
-,'Gandalf' : "Gandalf: developed by MycoMama Angela and Bjorn <p>It is a 'gift only culture'. It is to NEVER be sold. Only gifted</p><p>Gandalf finds you</p><p>An isolation of the t.a.m project Angela N Bjorn Andersen have worked on...constantly producing monster fruits first flush with a few smaller weirdos...this variant has broken 300 grams wet first flush but over 100 is fairly normal</p><p> it tends to be a shapeshifter because it's intended to be grown out, swabbed and gifted, so its genetics are a little wild.</p><p></p>"
+
+,'Pearly_Gates' : '<p>An isolation of the t.a.m project Michael Montgomery has worked on...with full tubs first flush.. beautiful blue caps and a pearly shimmer to the lumpy stipes..</p>\
+<p></p>'
+
+,'Gandalf' : "<p>Gandalf: developed by MycoMama Angela and Bjorn </p>\
+<p>It is a 'gift only culture'. It is to NEVER be sold. Only gifted</p>\
+<p>Gandalf finds you</p>\
+<p>An isolation of the t.a.m project Angela N Bjorn Andersen have worked on...constantly producing monster fruits first flush with a few smaller weirdos...this variant has broken 300 grams wet first flush but over 100 is fairly normal</p>\
+<p> it tends to be a shapeshifter because it's intended to be grown out, swabbed and gifted, so its genetics are a little wild.</p>\
+<p></p>"
+
 ,'Eye_Like' : 'info?'
-,'MVP' : '<p>One of the first isolations from the TAM project...full tub first flush of long stipe fruits with a gumdrop brown cap with a bit of a black halo to them..</p><p></p>'
-,'AMVP' : '<p>An abino mutation of the mvp dave isolated ..</p>'
+
+,'MVP' : '<p>MVP: (meaning?)<p>\
+<p>One of the first isolations from the TAM project...full tub first flush of long stipe fruits with a gumdrop brown cap with a bit of a black halo to them..</p>\
+<p></p>'
+
+,'AMVP' : '<p>An abino mutation of the mvp dave isolated ..</p>\
+<p></p>'
+
 ,'End_Game' : 'info?'
-,'Blue_J' : '<p>An isolation of the TAM project...even pin set of tiny fruits that break veil vary early but continue to grow for 7 day...maxing out at 3 inches in height </p><p></p>'
-,'Blue_Moon' : '<p>An isolation of the t.a.m project...growing in large clusters of ghost shaped fruits...dense fruits with heavy brusing"</p><p></p>'
-,'Hydra' : 'Hydra: <p>made by Angela and Bjorn,</p><p></p>'
-,'Omni' : 'Omni: <p>made by Angela and Bjorn,</p><p></p>'
-,'Shakti' : 'Shakti: <p>made by Angela and Bjorn,</p><p></p>'
-,'TW' : '<p>Tidal Wave: B+ x PE</p><p>Original cross by Doma(?)</p>'
-,'TePe' : '(name origin?): Tidalwave x PE'
-,'Enigma' : '<p>Enigma:</p><p>Isolation/mutation of TW</p><p>Gifting strain, propagation only by clone</p><p>because it does not sporulate</p>'
-,'APE' : 'APE: Albino PE <p></p><p></p>'
+
+,'Blue_J' : '<p>An isolation of the TAM project...even pin set of tiny fruits that break veil vary early but continue to grow for 7 day...maxing out at 3 inches in height </p>\
+<p></p>'
+
+,'Blue_Moon' : '<p>An isolation of the t.a.m project...growing in large clusters of ghost shaped fruits...dense fruits with heavy brusing"</p>\
+<p></p>'
+
+,'Hydra' : 'Hydra: <p>made by Angela and Bjorn,</p>\
+<p></p>'
+
+,'Omni' : 'Omni: <p>made by Angela and Bjorn,</p>\
+<p></p>'
+
+,'Shakti' : 'Shakti: <p>made by Angela and Bjorn,</p>\
+<p></p>'
+
+,'TW' : '<p>Tidal Wave: B+ x PE</p>\
+<p>Original cross by Doma(?)</p>\
+<p></p>'
+
+,'TePe' : 'TePe (name origin?): Tidalwave x PE'
+
+,'Enigma' : '<p>Enigma:</p>\
+<p>Isolation/mutation of TW</p>\
+<p>Gifting strain, propagation only by clone because it does not sporulate</p>'
+
+,'APE' : '<p>APE: Albino PE </p>\
+<p></p>'
+
 //template:
 //no spaces in first element! 
 // use <p> and </p> for line breaks
